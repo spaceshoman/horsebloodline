75b9cf27bc4895980254d02953317c434754d458
spaceshoman <sho.otake.spacex@gmail.com>
2026-05-30 08:12:53 +0000

    deploy: mobile CSS fix

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'keibakun-v3';
+const BASE = '/horsebloodline';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        if (r.ok) {
+          const clone = r.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        }
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
